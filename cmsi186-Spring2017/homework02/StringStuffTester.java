5106b1b955bc2a416805174a4d7e81da562fc9f7
B.J. Johnson <bjohnson05@gmail.com>
2017-01-25 17:11:38 -0800

    added StringStuffTester.java
    Empty boilerplate test harness

---
+++ cmsi186-Spring2017/homework02/StringStuffTester.java
@@ -0,0 +1,89 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  StringStuffTester.java
+ *  Purpose       :  A test harness file for testing out the methods in the "StringStuff.java" class
+ *  Author        :  B.J. Johnson
+ *  Date          :  2017-01-25
+ *  Description   :  This file provides the "test harness" for checking out the methods which are part of
+ *                   the homework02 assignment.  It also provides examples of proper documentation, and
+ *                   uses the source file header template as specified in the "Greeter.java" template file
+ *                   for use in CMSI 186, Spring 2017.
+ *  Notes         :  TEMPLATE FILE ONLY: Your job is to fill in as many test cases as you think are needed
+ *                   to thoroughly and completely test your StringStuff class.
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision History
+ *  ----------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *  @version 1.0.0  2017-01-25  B.J. Johnson  Initial writing and release
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+public class StringStuffTester {
+
+  /**
+   * the main method which calls all of the test methods in the class
+   * @param args String[] array containing command line parameters
+   * @return void
+   */
+   public static void main ( String [] args ) {
+
+      test_containsVowel();      // fill in how many tests
+      test_isPalindrome();       // fill in how many tests
+      test_evensOnly();          // fill in how many tests
+      test_oddsOnly();           // fill in how many tests
+      test_evensOnlyNoDupes();   // fill in how many tests
+      test_oddsOnlyNoDupes();    // fill in how many tests
+      test_reverse();            // fill in how many tests
+   }
+
+  /**
+   * test method to test out the operation of the containsVowel method
+   */
+   public static void test_containsVowel() {
+
+   }
+
+  /**
+   * test method to test out the operation of the isPalindrome method
+   */
+   public static void test_isPalindrome() {
+
+   }
+
+  /**
+   * test method to test out the operation of the evensOnly method
+   */
+   public static void test_evensOnly() {
+
+   }
+
+  /**
+   * test method to test out the operation of the oddsOnly method
+   */
+   public static void test_oddsOnly() {
+
+   }
+
+  /**
+   * test method to test out the operation of the evensOnlyNoDupes method
+   */
+   public static void test_evensOnlyNoDupes() {
+
+   }
+
+  /**
+   * test method to test out the operation of the oddsOnlyNoDupes method
+   */
+   public static void test_oddsOnlyNoDupes() {
+
+   }
+
+  /**
+   * test method to test out the operation of the reverse method
+   */
+   public static void test_reverse() {
+
+   }
+
+}
