09801a4f3b180348a6fcfac08030ae6d8d708d97
B.J. Johnson <bjohnson05@gmail.com>
2017-02-09 17:56:53 -0800

    New copy of DiceSet.java
    Original was based on Card.java, and had that old code in it.

--- cmsi186-Spring2017/homework03/DiceSet.java
+++ cmsi186-Spring2017/homework03/DiceSet.java
@@ -1,13 +1,22 @@
 /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- *  File name     :  Card.java
- *  Purpose       :  Provides a class describing a playing card
+ *  File name     :  DiceSet.java
+ *  Purpose       :  Provides a class describing a set of dice
  *  Author        :  B.J. Johnson
- *  Date          :  2017-02-06
- *  Description   :  This class provides everything needed (pretty much) to describe a playing card,
- *                   including its suit and rank, along with a couple of constructors.  There is a method
- *                   to output what this card actually *is*, generically, no matter what its suit and
- *                   rank might have been assigned.  There is a main method included [empty] for use as
- *                   a test harness if desired.
+ *  Date          :  2017-02-09
+ *  Description   :  This class provides everything needed (pretty much) to describe a set of dice.  The
+ *                   idea here is to have an implementing class that uses the Die.java class.  Includes
+ *                   the following:
+ *                   public DiceSet( int k, int n );                  // Constructor for a set of k dice each with n-sides
+ *                   public int sum();                                // Returns the present sum of this set of dice
+ *                   public void roll();                              // Randomly rolls all of the dice in this set
+ *                   public void rollIndividual( int i );             // Randomly rolls only the ith die in this set
+ *                   public int getIndividual( int i );               // Gets the value of the ith die in this set
+ *                   public void setIndividual( int i, int jValue );  // Sets the ith die in this set to the value of jValue
+ *                   public String toString();                        // Returns a stringy representation of this set of dice
+ *                   public static String toString( DiceSet ds );     // Classwide version of the preceding instance method
+ *                   public boolean isIdentical( DiceSet ds );        // Returns true iff this set is identical to the set ds
+ *                   public static void main( String[] args );        // The built-in test program for this class
+ *
  *  Notes         :  Stolen from Dr. Dorin pretty much verbatim, then modified to show some interesting
  *                   things about Java, and to add this header block and some JavaDoc comments.
  *  Warnings      :  None
@@ -17,36 +26,9 @@
  *  ---------------
  *            Rev      Date     Modified by:  Reason for change/modification
  *           -----  ----------  ------------  -----------------------------------------------------------
- *  @version 1.0.0  2017-02-06  B.J. Johnson  Initial writing and release
+ *  @version 1.0.0  2017-02-09  B.J. Johnson  Initial writing and release
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-public class Card {
-
-  /**
-   * public constants: ranks
-   */
-   public static final int ACE   = 1;
-   public static final int DEUCE = ACE   + 1;
-   public static final int TWO   = ACE   + 1;
-   public static final int THREE = TWO   + 1;
-   public static final int FOUR  = THREE + 1;
-   public static final int FIVE  = FOUR  + 1;
-   public static final int SIX   = FIVE  + 1;
-   public static final int SEVEN = SIX   + 1;
-   public static final int EIGHT = SEVEN + 1;
-   public static final int NINE  = EIGHT + 1;
-   public static final int TEN   = NINE  + 1;
-   public static final int JACK  = TEN   + 1;
-   public static final int KNAVE = TEN   + 1;
-   public static final int QUEEN = JACK  + 1;
-   public static final int KING  = QUEEN + 1;
-
-  /**
-   * public constants: suits
-   */
-   public static final int SPADES   = 1;
-   public static final int HEARTS   = SPADES   + 1;
-   public static final int DIAMONDS = HEARTS   + 1;
-   public static final int CLUBS    = DIAMONDS + 1;
+public class DiceSet {
 
   /**
    * private instance data
@@ -57,74 +39,77 @@ public class Card {
    // public constructor:
   /**
    * constructor
-   * @param rank int value containing THIS card's rank
-   * @param suit int value containing THIS card's suit
-   * @throws IllegalArgumentException
-   * @note parameters are checked for validity; invalid values throw "IllegalArgumentException"
-   * @return Constructors DON'T HAVE RETURN VALUES, NIMROD!
+   * @param  count int value containing total dice count
+   * @param  sides int value containing the number of pips on each die
+   * @throws IllegalArgumentException if one or both arguments don't make sense
+   * @note   parameters are checked for validity; invalid values throw "IllegalArgumentException"
    */
-   public Card( int rank, int suit ) {
-      if( (Card.ACE > rank | Card.KING < rank) | (Card.SPADES > suit | Card.CLUBS < suit) ) {
-         throw new IllegalArgumentException();
-      } else {
-         this.rank = rank;
-         this.suit = suit;
-      }
+   public DiceSet( int count, int sides ) {
    }
 
   /**
-   * @return the suit of THIS card instance
+   * @return the sum of all the dice values in the set
    */
-   public int getSuit() {
+   public int sum() {
       return this.suit;
    }
 
   /**
-   * @return the rank of THIS card instance
+   * Randomly rolls all of the dice in this set
    */
-   public int getRank() {
-      return this.rank;
+   public void roll() {
+      
    }
 
   /**
-   * @return Public Instance method that returns a String representation of THIS card instance
+   * Randomly rolls a single die of the dice in this set
+   * @param  dieIndex int of which die to roll
+   * @trhows IllegalArgumentException if the index is out of range
+   */
+   public void rollIndividual( int dieIndex ) {
+      
+   }
+
+  /**
+   * Gets the value of the die in this set indexed by 'dieIndex'
+   * @param  dieIndex int of which die to roll
+   * @trhows IllegalArgumentException if the index is out of range
+   */
+   public void getIndividual( int dieIndex ) {
+      return -999;
+   }
+
+  /**
+   * Gets the value of the die in this set indexed by 'dieIndex'
+   * @param  dieIndex int of which die to set the value of
+   * @param  dieValue int of the value to set for that die
+   * @trhows IllegalArgumentException if the index is out of range
+   */
+   public void setIndividual( int dieIndex, int dieValue ) {
+      
+   }
+
+  /**
+   * @return Public Instance method that returns a String representation of the DiceSet instance
    */
    public String toString() {
       String result = "";
-      switch( this.rank ) {
-         case 1 : result = "Ace of "; break;
-         case 2 : result = "Deuce of "; break;
-         case 3 : result = "Three of "; break;
-         case 4 : result = "Four of "; break;
-         case 5 : result = "Five of "; break;
-         case 6 : result = "Six of "; break;
-         case 7 : result = "Seven of "; break;
-         case 8 : result = "Eight of "; break;
-         case 9 : result = "Nine of "; break;
-         case 10 : result = "Ten of "; break;
-         case 11 : result = "Jack of "; break;
-         case 12 : result = "Queen of "; break;
-         case 13 : result = "King of "; break;
-         default : throw new UnsupportedOperationException();
-      }
-
-      switch( this.suit ) {
-         case  1 : result += "Spades"; break;
-         case  2 : result += "Hearts"; break;
-         case  3 : result += "Diamonds"; break;
-         case  4 : result += "Clubs"; break;
-         default : throw new UnsupportedOperationException();
-      }
       return result;
    }
 
   /**
-   * @return Class-wide method that returns a String representation of THIS card instance
+   * @return Class-wide version of the preceding instance method
    */
-   public static String toString( Card c ) {
-      return c.toString();
+   public static String toString( DiceSet ds );
+      return "";
    }
 
+  /**
+   * @return  tru iff this set is identical to the set passed as an argument
+   */
+   public boolean isIdentical( DiceSet ds ) {
+      return true;
+   }
   /**
    * A little test main to check things out
    */
