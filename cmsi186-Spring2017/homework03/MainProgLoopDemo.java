378b6f0fd694a40e863ed1453949a993d67ee927
B.J. Johnson <bjohnson05@gmail.com>
2017-02-14 11:43:29 -0800

    Added MaiinProgLoopDemo.java
    Demonstrator for a "good" endless loop for a TUI to use with the Yahtzee program assignment.

---
+++ cmsi186-Spring2017/homework03/MainProgLoopDemo.java
@@ -0,0 +1,47 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  MainProgLoopDemo.java
+ *  Purpose       :  Demonstrates the use of input from a command line for use with Yahtzee
+ *  Author        :  B.J. Johnson
+ *  Date          :  2017-02-14
+ *  Description   :  
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision Histor
+ *  ---------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *  @version 1.0.0  2017-02-14  B.J. Johnson  Initial writing and release
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class MainProgLoopDemo{
+
+   public static void main( String args[] ) {
+      System.out.println( "\n   Welcome to the MainProgLoopDemo!!\n" );
+      System.out.println( "     Press the 'q' key to quit the program." );
+
+     // This line uses the two classes to assemble an "input stream" for the user to type
+     // text into the program
+      BufferedReader input = new BufferedReader( new InputStreamReader( System.in ) );
+      while( true ) {
+         System.out.print( ">>" );
+         String inputLine = null;
+         try {
+            inputLine = input.readLine();
+            if( 0 == inputLine.length() ) {
+               System.out.println("enter some text!:");
+            }
+            System.out.println( "   You typed: " + inputLine );
+            if( 'q' == inputLine.charAt(0) ) {
+               break;
+            }         }
+         catch( IOException ioe ) {
+            System.out.println( "Caught IOException" );
+         }
+      }
+   }
+}
