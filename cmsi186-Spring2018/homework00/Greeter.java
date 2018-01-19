59a9e9a1b8bb19a6ffaba44cba15809c2c09588c
B.J. Johnson <bjohnson05@gmail.com>
2018-01-18 17:06:12 -0800

    Committing demo of Greeter.java

--- cmsi186-Spring2018/homework00/Greeter.java
+++ cmsi186-Spring2018/homework00/Greeter.java
@@ -1,22 +1,5 @@
-/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- *  File name     :  Greeter.java
- *  Purpose       :  Says "Hello world"; first program; provides a header template for Java programs
- *  Author        :  B.J. Johnson
- *  Date          :  2017-01-02
- *  Description   :  See purpose (in this case; other files will have more details)
- *  Notes         :  None
- *  Warnings      :  None
- *  Exceptions    :  None
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- *  Revision Histor
- *  ---------------
- *            Rev      Date     Modified by:  Reason for change/modification
- *           -----  ----------  ------------  -----------------------------------------------------------
- *  @version 1.0.0  2017-01-02  B.J. Johnson  Initial writing and release
- *  @version 1.0.1  2017-12-25  B.J. Johnson  Updated for Spring 2018 version
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-// you will need to insert your code here
-//  to compile, open a terminal window and type "javac Greeter.java" then press "Enter"
-//  to execute your code, in the terminal window type "java Greeter" then press "Enter"
-// you can remove these line comments and replace them with your code
+public class Greeter {
+   public static void main( String[] args ) {
+      System.out.println( "Hello World!" );
+   }
+}
